72ab5c79191558d6940a34aca4f002e69b6b5510
Rafael2604 <cesaraugusto.nobre@hotmail.com>
2019-04-06 15:05:55 -0300

    Maratona Java

---
+++ src/ExercicioControleFluxo3.java
@@ -0,0 +1,11 @@
+public class ExercicioControleFluxo3 {
+    public static void main(String[] args) {
+        //Imprima todos os numeros Pares de 0 ate 100000
+        int valor = 500000;
+                for(int i=0; i<=valor; i++){
+                    if(i%2!=0){
+                        System.out.println(i);
+                    }
+                }
+    }
+}
---
+++ src/ExerciciosControleFluxo2.java
@@ -0,0 +1,23 @@
+public class ExerciciosControleFluxo2 {
+    public static void main(String[] args) {
+        //Crie um Switch que dado um Valor de 1 a 7
+        //considerando 1 domingo imprima se é dia util ou final de semana
+        byte dia = 0;
+        switch (dia) {
+            case 1:
+                case 7:
+                System.out.println("fds");
+                break;
+                case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+               System.out.println("DIA util");
+            break;
+            default:
+                System.out.println("Opção Invalida");
+        }
+
+    }
+}
